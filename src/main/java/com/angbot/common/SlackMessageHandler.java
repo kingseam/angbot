90573dab0fdae361b9ca99b8bf777f81fe00e100
164114 <164114@shinsegae.com>
2018-03-13 10:23:46 +0900

    다시 sleep으로 변경. 웹소켓 공부좀하고.. 다시

--- src/main/java/com/angbot/common/SlackMessageHandler.java
+++ src/main/java/com/angbot/common/SlackMessageHandler.java
@@ -6,7 +6,6 @@ import java.util.Map.Entry;
 import java.util.StringTokenizer;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 import javax.websocket.Session;
 
@@ -85,13 +84,10 @@ public class SlackMessageHandler implements MessageHandler {
 							Gson gson = new Gson();
 							String json = gson.toJson(_temp);
 							System.out.println(json);
-							Future<Void> req = userSession.getAsyncRemote().sendText(json);
-							req.get();
-							if(req.isDone()){
-								result.put("text",((CommCommand) SlackCmdCache.cmdMap.get(cmd)).run(token));
-							}
+							userSession.getAsyncRemote().sendText(json);
+							Thread.sleep(2000);
+							result.put("text",((CommCommand) SlackCmdCache.cmdMap.get(cmd)).run(token));
 						}else if (SlackCmdCache.cmdMap.containsKey(cmd)) {
-							System.out.println("이게들어오나?");
 							String resultMsg = "`해당 " + cmd + " 명령은 이미 수행중입니다.`";
 							if (!((CommCommand) SlackCmdCache.cmdMap.get(cmd)).isState()) {
 								resultMsg = ((CommCommand) SlackCmdCache.cmdMap.get(cmd)).run(token);
